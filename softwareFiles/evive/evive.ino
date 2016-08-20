62cff2b6f27a6b9de47a31b3aea5987eaffbe8fe
Dhrupal R Shah <dhrupal93@gmail.com>
2016-08-20 15:04:11 +0530

    Mentioned IDE 1.6.6 in Comments

--- softwareFiles/evive/evive.ino
+++ softwareFiles/evive/evive.ino
@@ -1,4 +1,12 @@
-#include "evive.h"
+/*
+ * This firmware is developed for evive [http://evive.cc].
+ * This is licensed under Creative Commons 4.0
+ * Written by Dhrupal R Shah and Akshat Agarwal for Agilo Technology.
+ * Last update: 2016, Aug 20
+ * 
+ * NOTE: Works with Arduino IDE 1.6.6 or later
+ */
+ #include "evive.h"
 
 void setup(){
   Serial.begin(BAUDRATE);
